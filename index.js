da2c7d682edc632858dcb0cfba6091228f6a85f7
Uyedacb <uyedacb@uw.edu>
2018-09-09 23:19:39 -0700

    change node -v to 8.1 for ask-sdk, fix variable snytax error

--- index.js
+++ index.js
@@ -6,18 +6,17 @@ const LaunchRequestHandler = {
     return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
   },
   handle(handlerInput) {
-    const speechText = 'Welcome to Trak, you can modify, look at, and add time to your activities. Try saying, add one minute to learning!';
-
+    const speechText = 'Welcome to Act Track, you can modify, look at, and add time to your activities try saying, add one minute to learning!';
+    const reprompt = 'help me'
     return handlerInput.responseBuilder
       .speak(speechText)
-      .reprompt(speechText)
-      .withSimpleCard('Trak', speechText)
+      .reprompt(reprompt)
       .getResponse();
-  }
-}
+  },
+};
 
 const addTimeIntentHandler = {
-  canHandle(canHandle) {
+  canHandle(handlerInput) {
     return handlerInput.requestEnvelope.request.type === 'IntentRequest'
       && handlerInput.requestEnvelope.request.intent.name === 'addTimeIntent';
   },
@@ -25,20 +24,79 @@ const addTimeIntentHandler = {
     // insert code to update dynamoDB for user here
     const activity = handlerInput.requestEnvelope.request.intent.slots.activity.value;
     const time = handlerInput.requestEnvelope.request.intent.slots.time.value;
-    const speechText = `${time} has been added to ${activity}`
+    const speechText = `${time} has been added to ${activity}`;
     // probably do an if statement to check if activity exists or if time is valid
     // and return some type of speechText error that reprompts the user to fix their mistake
     // return speechText that shows intent was handled
     return handlerInput.responseBuilder
       .speak(speechText)
+      .getResponse();
+  },
+};
+
+const HelpIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
+  },
+  handle(handlerInput) {
+    const speechText = 'You can say hello to me!';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
       .reprompt(speechText)
-      .withSimpleCard('Trak', speechText)
+      .withSimpleCard('Hello World', speechText)
+      .getResponse();
+  },
+};
+
+const CancelAndStopIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent'
+        || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
+  },
+  handle(handlerInput) {
+    const speechText = 'Goodbye!';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .withSimpleCard('Hello World', speechText)
       .getResponse();
   }
-}
+};
+
+const SessionEndedRequestHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
+  },
+  handle(handlerInput) {
+    //any cleanup logic goes here
+    return handlerInput.responseBuilder.getResponse();
+  }
+};
+
+const ErrorHandler = {
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput, error) {
+    console.log(`Error handled: ${error.message}`);
+
+    return handlerInput.responseBuilder
+      .speak('Sorry, I can\'t understand the command. Please say again.')
+      .reprompt('Sorry, I can\'t understand the command. Please say again.')
+      .getResponse();
+  },
+};
 
 exports.handler = Alexa.SkillBuilders.custom()
   .addRequestHandlers(
     LaunchRequestHandler,
-    addTimeIntentHandler) // errorhandler does not exist
+    addTimeIntentHandler,
+    HelpIntentHandler,
+    CancelAndStopIntentHandler,
+    SessionEndedRequestHandler
+  )
+  .addErrorHandlers(ErrorHandler) // errorhandler does not exist
   .lambda();
